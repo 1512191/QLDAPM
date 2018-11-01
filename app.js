6697c095ff8400da1f10d9d4578e9184da88f7fc
1512191 <1512191@student.hcmus.edu.vn>
2018-11-01 19:40:43 +0700

    question done

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var exphbs = require('express-handlebars');
+var exphbs_section = require('express-handlebars-sections');
+var path = require('path');
+
+var cauHoiController = require('./controllers/cauhoiController');
+var app = express();
+app.engine('hbs', exphbs({
+    defaultLayout: 'admin',
+
+    layoutsDir: 'views/_layouts/',
+    helpers: {
+        section: exphbs_section()
+    }
+}));
+app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
+app.use(express.static(
+    path.resolve(__dirname, 'public')
+));
+app.use('/admin/cauhoi/', cauHoiController);
+app.get('/', (req, res) => {
+    res.render('admin/nguoidung/index');
+});
+
+
+// app.use("/", homeController);
+
+app.listen(3000, () => {
+    console.log('server running on port 3000');
+});
